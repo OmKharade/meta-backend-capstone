ced889aacb540dfef256a751e20e8afe7e41a9d3
OmKharade <133861833+OmKharade@users.noreply.github.com>
2024-06-27 18:32:36 +0530

    set up the table booking api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Menu
-from .serializers import MenuSerializer
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -12,7 +13,11 @@ class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
-
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
