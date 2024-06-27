869e320a0ccc862951e37dd2d298c508e2af13c3
OmKharade <133861833+OmKharade@users.noreply.github.com>
2024-06-27 19:48:18 +0530

    secure the table booking api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -10,6 +10,7 @@ def index(request):
     return render(request, "index.html", {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
